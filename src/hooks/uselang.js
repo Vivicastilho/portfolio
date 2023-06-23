a366da53b73e1fba028ad32c6789f9d1cbe6c643
Viviane Castilho <99738803+Vivicastilho@users.noreply.github.com>
2023-06-23 12:02:04 -0300

    Create uselang.js

---
+++ src/hooks/uselang.js
@@ -0,0 +1,96 @@
+export const langTokens = (mode) =>
+  mode === "En"
+    ? {
+      //teste
+        navBar: {
+          home: "Home",
+          about: "About me",
+          skills: "My Skills",
+          portfolio: "Portfolio",
+          contact: "Contact",
+          pt: "Portuguese",
+          en: "English",
+        },
+        inicio: {
+          welcome: "Hi, I am",
+          h1: "Viviane Castilho",
+          h3: "UI/UX & Web Designer ",
+          contact:
+            "As a freelancer, I offer website creation services with expertise in prototyping, wireframing, and user experience (UX) design. Additionally, I have intermediate knowledge of English, allowing me to serve international clients. With experience in interface design and team leadership, I am ready to assist you in developing an effective and appealing website. Please reach out to discuss your idea and turn it into an online reality.",
+          rights: "Copyright © Todos os direitos reservados",
+        },
+        aboutMe: {
+          header: "About Me",
+          header2: "Skills",
+          header3: "Knowledge",
+          aboutMeP1: `I have a passion for reading and exploring new cultures. I lived in Ireland for 5 years, where I not only learned English but also gained extensive knowledge of Irish culture. Currently, I am undergoing training to become a web developer, specializing in HTML, CSS, JavaScript, and React, along with expertise in UI/UX design and Figma. I have hands-on experience in creating wireframes, prototypes, and designs for both web and mobile applications. Additionally, my background includes problem-solving and teamwork skills, as well as leadership and communication abilities honed through my international experiences. I am eagerly prepared to take on fresh challenges and expand my expertise in web development, UI/UX, and related fields. Presently, I am studying UI/UX through a course at Coder House, alongside the Agile Scrum methodology.`,
+          aboutMeP2:
+            "My hobbies are cooking, strategie games, coding and animes I currently own a Pizzaria and I´m working hard to get in to the WoC (World of Code). I hope you like what you see, everything is done with the utmost care, If you have any question go to the “contact me” page, I´ll be happy to awnser. ",
+          c1: "User Experience Design",
+          c2: "Interface Design",
+          c3: "Website Development",
+          c4: "Intermediate English",
+          c5: "Team Leadership",
+          c6: "Landing pages",
+        },
+        portfolio: {
+          header: "Portfolio",
+          header2: "My Work",
+          see: "Check it out",
+        },
+        trabalhos: {
+          DiToscana: ["Di Toscana Pizzaria","In progress... Website  design being make in Figma for a pizzeria in Caldas Novas, Goiás."],
+          SimpleNotes: ["Simple Notes","Creation made with Figma tool through Rodrigo Gonçalves' web design course."],
+          LehPort: ["Portfolio","Project created for a portfolio, made in Figma."],
+          Cakes: ["Delicious Cake","Creation made with Figma tool for a handmade bakery, both web and mobile."],
+          Kindle: ["Kindle","Test UI design project created for a technology-themed company. Includes Landing Page, E-mail Marketing, and Mobile design."],
+          MyPort: ["Personal Portfolio","Personal portfolio created in Figma."],
+        },
+      }
+    : {
+        navBar: {
+          home: "Inicio ",
+          about: "Sobre",
+          skills: "Habilidades",
+          portfolio: "Portfólio",
+          contact: "Contato",
+          pt: "Português",
+          en: "Inglês",
+        },
+        inicio: {
+          welcome: "Olá, eu sou",
+          h1: "Viviane Castilho",
+          h3: "UI/UX & Web Designer ",
+          contact:
+            "Como freelancer, ofereço serviços de criação de sites com habilidades em elaboração de protótipos, wireframes e design de experiência do usuário (UX). Além disso, possuo conhecimento intermediário em inglês, o que me permite atender clientes internacionais. Com experiência em design de interfaces e liderança de equipe, estou pronto para ajudar você a desenvolver um site eficaz e atraente. Entre em contato para discutirmos sua ideia e transformá-la em uma realidade online.",
+          rights: "Copyright © Todos os direitos reservados",
+        },
+        aboutMe: {
+          header: "Sobre Mim",
+          header2: "Habilidades",
+          header3: "Competencias",
+          aboutMeP1:
+            "Eu tenho uma paixão pela leitura e por conhecer novas culturas. Morei na Irlanda por 5 anos, onde não apenas aprendi inglês, mas também adquiri um amplo conhecimento da cultura irlandesa. Atualmente, estou em formação para me tornar uma desenvolvedora web, com habilidades em HTML, CSS, JavaScript e React, além de expertise em UI/UX Design e Figma. Tenho experiência prática na criação de wireframes, protótipos e designs para aplicações web e mobile. Além disso, possuo habilidades em resolução de problemas e trabalho em equipe, bem como liderança e comunicação adquiridas por meio das minhas experiências internacionais. Estou preparada e entusiasmada para enfrentar novos desafios e expandir meu conhecimento em desenvolvimento web, UI/UX e áreas relacionadas. Atualmente, estou estudando UI/UX por meio de um curso na Coder House, juntamente com a metodologia Ágil Scrum.",
+          aboutMeP2:
+            "Meus hobbies são cozinhar, jogos RPG, codar e assistir animes. No momento sou também proprietário de uma pizzaria, tenho trabalhado e estudado muito para entrar no mundo do código. Espero que goste do que veja, pois tudo o que está aqui foi feito com muito cuidado e dedicação. Se tiver alguma dúvida, entre em contato que terei o prazer em responder.",
+          c1: "Design de experiência do usuário",
+          c2: "Design de interfaces",
+          c3: "Desenvolvimento de sites",
+          c4: "Inglês intermediário",
+          c5: "Liderança de equipe",
+          c6: "Landing pages",
+        },
+        portfolio: {
+          header: "Portfólio",
+          header2: "Meus Trabalhos",
+          see: "Veja Mais",
+        },
+        trabalhos: {
+          DiToscana: ["Di Toscana Pizzaria","Em andamento.... Site está sendo desenvolvido no Figma para uma pizzaria em Caldas Novas - Goiás."],
+          SimpleNotes: ["Simple Notes","Criação feita com a ferramenta Figma pelo curso do Rodrigo Gonçalves, web."],
+          LehPort: ["Portfólio","Projeto criado para um Portifólio realizado no Figma."],
+          Cakes: ["Delicious Cake","Criação feita com a ferramenta Figma para uma confeitaria artesanal, web e mobile."],
+          Kindle: ["Kindle","Projeto Teste de UI design desenvolvido para uma empresa com o tema tecnologia. Landing Page, E-mail Marketing e Mobile. "],
+          MyPort: ["Meu Portfólio","Desenvolvimento de Portfólio pessoal realizado no Figma."],
+        },
+      };
