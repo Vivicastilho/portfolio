4ddfbb0814ec0f9d3d0394de6d0cc94a1d5ad6f7
Viviane Castilho <99738803+Vivicastilho@users.noreply.github.com>
2023-09-21 22:04:00 -0300

    Atualização

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import AlePortifolio from "../../assets/AlePortfolio.png";
+// import AlePortifolio from "../../assets/AlePortfolio.png";
 import Cake from "../../assets/DeliciousCake.jpg";
 import Pizzaria from "../../assets/DiToscana.png";
 import Kindle from "../../assets/Kindle.jpg";
-import Notes from "../../assets/SimpleNotes.jpg";
-import ViPortifolio from "../../assets/ViPortfolio.png";
+// import Notes from "../../assets/SimpleNotes.jpg";
+// import ViPortifolio from "../../assets/ViPortfolio.png";
+import CaseMera from "../../assets/CaseMera.png";
 import { Footer } from "../../components/Footer";
 import { Jobs } from "../../components/Jobs";
 import { Nav } from "../../components/Nav";
@@ -24,25 +25,32 @@ export const Portfolio = () => {
         <h1>{portfolio.header}</h1>
         <h4>{portfolio.header}</h4>
         <Jobs
+          image={CaseMera}
+          title={trabalhos.CaseMera[0]}
+
+          description={trabalhos.CaseMera[1]}
+          link="https://www.behance.net/gallery/178787583/Aplicativo-de-Monitoramento"
+        />
+        <Jobs
           image={Pizzaria}
           title={trabalhos.DiToscana[0]}
           description={trabalhos.DiToscana[1]}
           link="https://www.figma.com/file/OAAJVElr6qakdfZNjbWYnI/Pizzaria-Di-Toscana?type=design&node-id=2-3&mode=design&t=LygYtRZ8iuxV4cUe-0"
         />
-        <Jobs
+        {/* <Jobs
           image={Notes}
           title={trabalhos.SimpleNotes[0]}
 
           description={trabalhos.SimpleNotes[1]}
           link="https://www.figma.com/file/WLnXWHihUKlI5NvHsF48Qg/Simple-Notes?type=design&node-id=0-1&mode=design&t=LygYtRZ8iuxV4cUe-0"
-        />
-        <Jobs
+        /> */}
+        {/* <Jobs
           image={AlePortifolio}
           title={trabalhos.LehPort[0]}
 
           description={trabalhos.LehPort[1]}
           link="https://www.figma.com/file/n3bHYM36CZgRNGGLuLJAq9/LehPortfolio?type=design&node-id=1-1552&mode=design&t=LygYtRZ8iuxV4cUe-0"
-        /> 
+        />  */}
         <Jobs
           image={Cake}
           title={trabalhos.Cakes[0]}
@@ -57,13 +65,13 @@ export const Portfolio = () => {
           description={trabalhos.Kindle[1]}
           link="https://www.figma.com/file/gjEdGjXwpskPj3PYxvlB9e/TesteTheAlfred?type=design&node-id=3-3&mode=design&t=LygYtRZ8iuxV4cUe-0"
         /> 
-        <Jobs
+        {/* <Jobs
           image={ViPortifolio}
           title={trabalhos.MyPort[0]}
 
           description={trabalhos.MyPort[1]}
           link="https://www.figma.com/file/jUUX9tfTlltsQQKZQrnWek/Portfolio2?type=design&node-id=10-3459&mode=design&t=LygYtRZ8iuxV4cUe-0"
-        />     
+        />      */}
     
       </Content>
 
